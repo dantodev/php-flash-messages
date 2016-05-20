f44ba667cc35029dee7f04091a4d57f510702dfa
daniel <kontakt@daniel-kahl.de>
2016-05-20 16:34:06 +0200

    * add PSR-2 compatibility #7

--- src/FlashMessages.php
+++ src/FlashMessages.php
@@ -1,361 +1,362 @@
 <?php namespace Dtkahl\FlashMessages;
 class FlashMessages
 {
-  
-  const TYPE_ERROR = "error";
-  const TYPE_WARNING = "warning";
-  const TYPE_INFO ="info";
-  const TYPE_SUCCESS = "success";
 
-  private $prev;
-  private $storage;
+    const TYPE_ERROR = "error";
+    const TYPE_WARNING = "warning";
+    const TYPE_INFO = "info";
+    const TYPE_SUCCESS = "success";
 
-  /**
-   * FlashMessages constructor.
-   * @param array|null $storage
-   * @param string $key
-   */
-  public function __construct(array &$storage = null, $key = "flash")
-  {
-    if (is_null($storage)) {
-      $storage = &$_SESSION;
-    } elseif (!is_array($storage) && !$storage instanceof \ArrayAccess){
-      throw new \InvalidArgumentException("Given storage must be array or extend ArrayAccess.");
+    private $prev;
+    private $storage;
+
+    /**
+     * FlashMessages constructor.
+     * @param array|null $storage
+     * @param string $key
+     */
+    public function __construct(array &$storage = null, $key = "flash")
+    {
+        if (is_null($storage)) {
+            $storage = &$_SESSION;
+        } elseif (!is_array($storage) && !$storage instanceof \ArrayAccess) {
+            throw new \InvalidArgumentException("Given storage must be array or extend ArrayAccess.");
+        }
+        $this->prev = array_key_exists($key, $storage) ? (array)$storage[$key] : [];
+
+        $storage[$key] = [];
+        $this->storage = &$storage[$key];
     }
-    $this->prev = array_key_exists($key, $storage) ? (array) $storage[$key] : [];
 
-    $storage[$key] = [];
-    $this->storage = &$storage[$key];
-  }
-  /**
-   * @param $type
-   * @param $key
-   * @param $default
-   * @return mixed
-   */
-  public function get($type, $key, $default = null)
-  {
-    return $this->has($type, $key) ? $this->prev[$type][$key] : $default;
-  }
-
-  /**
-   * @param $type
-   * @param $key
-   * @param $value
-   * @return $this
-   */
-  public function set($type, $key, $value)
-  {
-    if (!array_key_exists($type, $this->storage)) {
-      $this->storage[$type] = [];
-    };
-    $this->storage[$type][$key] = $value;
-    return $this;
-  }
-
-  /**
-   * @param $type
-   * @return array
-   */
-  public function getAll($type)
-  {
-    return array_key_exists($type, $this->prev) ? $this->prev[$type] : [];
-  }
-
-  /**
-   * @return array
-   */
-  public function getAllTypes()
-  {
-    return $this->prev;
-  }
-
-  /**
-   * @param $type
-   * @param $key
-   * @return $this
-   */
-  public function remove($type, $key)
-  {
-    if (array_key_exists($type, $this->storage) && array_key_exists($key, $this->storage[$type])) {
-      unset($this->storage[$type][$key]);
+    /**
+     * @param $type
+     * @param $key
+     * @param $default
+     * @return mixed
+     */
+    public function get($type, $key, $default = null)
+    {
+        return $this->has($type, $key) ? $this->prev[$type][$key] : $default;
     }
-    return $this;
-  }
 
-  /**
-   * @param $type
-   * @return $this
-   */
-  public function removeAll($type)
-  {
-    if (array_key_exists($type, $this->storage)) {
-      $this->storage[$type] = [];
+    /**
+     * @param $type
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function set($type, $key, $value)
+    {
+        if (!array_key_exists($type, $this->storage)) {
+            $this->storage[$type] = [];
+        };
+        $this->storage[$type][$key] = $value;
+        return $this;
     }
-    return $this;
-  }
 
-  /**
-   * @param $type
-   * @param $key
-   * @return bool
-   */
-  public function has($type, $key)
-  {
-    return array_key_exists($type, $this->prev) && array_key_exists($key, $this->prev[$type]);
-  }
+    /**
+     * @param $type
+     * @return array
+     */
+    public function getAll($type)
+    {
+        return array_key_exists($type, $this->prev) ? $this->prev[$type] : [];
+    }
 
-  public function hasAny($type)
-  {
-    return !empty($this->getAll($type));
-  }
+    /**
+     * @return array
+     */
+    public function getAllTypes()
+    {
+        return $this->prev;
+    }
 
-  /**
-   * @param $key
-   * @param $default
-   * @return mixed
-   */
-  public function getError($key, $default = null)
-  {
-    return $this->get(self::TYPE_ERROR, $key, $default);
-  }
+    /**
+     * @param $type
+     * @param $key
+     * @return $this
+     */
+    public function remove($type, $key)
+    {
+        if (array_key_exists($type, $this->storage) && array_key_exists($key, $this->storage[$type])) {
+            unset($this->storage[$type][$key]);
+        }
+        return $this;
+    }
 
-  /**
-   * @param $key
-   * @param $value
-   * @return $this
-   */
-  public function setError($key, $value)
-  {
-    return $this->set(self::TYPE_ERROR, $key, $value);
-  }
+    /**
+     * @param $type
+     * @return $this
+     */
+    public function removeAll($type)
+    {
+        if (array_key_exists($type, $this->storage)) {
+            $this->storage[$type] = [];
+        }
+        return $this;
+    }
 
-  /**
-   * @param $key
-   * @return bool
-   */
-  public function hasError($key)
-  {
-    return $this->has(self::TYPE_ERROR, $key);
-  }
+    /**
+     * @param $type
+     * @param $key
+     * @return bool
+     */
+    public function has($type, $key)
+    {
+        return array_key_exists($type, $this->prev) && array_key_exists($key, $this->prev[$type]);
+    }
 
-  /**
-   * @return bool
-   */
-  public function hasAnyError()
-  {
-    return $this->hasAny(self::TYPE_ERROR);
-  }
+    public function hasAny($type)
+    {
+        return !empty($this->getAll($type));
+    }
 
-  /**
-   * @return array
-   */
-  public function getAllError()
-  {
-    return $this->getAll(self::TYPE_ERROR);
-  }
+    /**
+     * @param $key
+     * @param $default
+     * @return mixed
+     */
+    public function getError($key, $default = null)
+    {
+        return $this->get(self::TYPE_ERROR, $key, $default);
+    }
 
-  /**
-   * @param $key
-   * @return $this
-   */
-  public function removeError($key)
-  {
-    return $this->remove(self::TYPE_ERROR, $key);
-  }
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setError($key, $value)
+    {
+        return $this->set(self::TYPE_ERROR, $key, $value);
+    }
 
-  /**
-   * @return $this
-   */
-  public function removeAllError()
-  {
-    return $this->removeAll(self::TYPE_ERROR);
-  }
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function hasError($key)
+    {
+        return $this->has(self::TYPE_ERROR, $key);
+    }
 
-  /**
-   * @param $key
-   * @param $default
-   * @return mixed
-   */
-  public function getWarning($key, $default = null)
-  {
-    return $this->get(self::TYPE_WARNING, $key, $default);
-  }
+    /**
+     * @return bool
+     */
+    public function hasAnyError()
+    {
+        return $this->hasAny(self::TYPE_ERROR);
+    }
 
-  /**
-   * @param $key
-   * @param $value
-   * @return $this
-   */
-  public function setWarning($key, $value)
-  {
-    return $this->set(self::TYPE_WARNING, $key, $value);
-  }
+    /**
+     * @return array
+     */
+    public function getAllError()
+    {
+        return $this->getAll(self::TYPE_ERROR);
+    }
 
-  /**
-   * @param $key
-   * @return bool
-   */
-  public function hasWarning($key)
-  {
-    return $this->has(self::TYPE_WARNING, $key);
-  }
+    /**
+     * @param $key
+     * @return $this
+     */
+    public function removeError($key)
+    {
+        return $this->remove(self::TYPE_ERROR, $key);
+    }
 
-  /**
-   * @return bool
-   */
-  public function hasAnyWarning()
-  {
-    return $this->hasAny(self::TYPE_WARNING);
-  }
+    /**
+     * @return $this
+     */
+    public function removeAllError()
+    {
+        return $this->removeAll(self::TYPE_ERROR);
+    }
 
-  /**
-   * @return array
-   */
-  public function getAllWarning()
-  {
-    return $this->getAll(self::TYPE_WARNING);
-  }
+    /**
+     * @param $key
+     * @param $default
+     * @return mixed
+     */
+    public function getWarning($key, $default = null)
+    {
+        return $this->get(self::TYPE_WARNING, $key, $default);
+    }
 
-  /**
-   * @param $key
-   * @return $this
-   */
-  public function removeWarning($key)
-  {
-    return $this->remove(self::TYPE_WARNING, $key);
-  }
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setWarning($key, $value)
+    {
+        return $this->set(self::TYPE_WARNING, $key, $value);
+    }
 
-  /**
-   * @return $this
-   */
-  public function removeAllWWarning()
-  {
-    return $this->removeAll(self::TYPE_WARNING);
-  }
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function hasWarning($key)
+    {
+        return $this->has(self::TYPE_WARNING, $key);
+    }
 
-  /**
-   * @param $key
-   * @param $default
-   * @return mixed
-   */
-  public function getSuccess($key, $default = null)
-  {
-    return $this->get(self::TYPE_SUCCESS, $key, $default);
-  }
+    /**
+     * @return bool
+     */
+    public function hasAnyWarning()
+    {
+        return $this->hasAny(self::TYPE_WARNING);
+    }
 
-  /**
-   * @param $key
-   * @param $value
-   * @return $this
-   */
-  public function setSuccess($key, $value)
-  {
-    return $this->set(self::TYPE_SUCCESS, $key, $value);
-  }
+    /**
+     * @return array
+     */
+    public function getAllWarning()
+    {
+        return $this->getAll(self::TYPE_WARNING);
+    }
 
-  /**
-   * @param $key
-   * @return bool
-   */
-  public function hasSuccess($key)
-  {
-    return $this->has(self::TYPE_SUCCESS, $key);
-  }
+    /**
+     * @param $key
+     * @return $this
+     */
+    public function removeWarning($key)
+    {
+        return $this->remove(self::TYPE_WARNING, $key);
+    }
 
-  /**
-   * @return bool
-   */
-  public function hasAnySuccess()
-  {
-    return $this->hasAny(self::TYPE_SUCCESS);
-  }
+    /**
+     * @return $this
+     */
+    public function removeAllWWarning()
+    {
+        return $this->removeAll(self::TYPE_WARNING);
+    }
 
-  /**
-   * @return array
-   */
-  public function getAllSuccess()
-  {
-    return $this->getAll(self::TYPE_SUCCESS);
-  }
+    /**
+     * @param $key
+     * @param $default
+     * @return mixed
+     */
+    public function getSuccess($key, $default = null)
+    {
+        return $this->get(self::TYPE_SUCCESS, $key, $default);
+    }
 
-  /**
-   * @param $key
-   * @return $this
-   */
-  public function removeSuccess($key)
-  {
-    return $this->remove(self::TYPE_SUCCESS, $key);
-  }
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setSuccess($key, $value)
+    {
+        return $this->set(self::TYPE_SUCCESS, $key, $value);
+    }
 
-  /**
-   * @return $this
-   */
-  public function removeAllSuccess()
-  {
-    return $this->removeAll(self::TYPE_SUCCESS);
-  }
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function hasSuccess($key)
+    {
+        return $this->has(self::TYPE_SUCCESS, $key);
+    }
 
-  /**
-   * @param $key
-   * @param $default
-   * @return mixed
-   */
-  public function getInfo($key, $default = null)
-  {
-    return $this->get(self::TYPE_INFO, $key, $default);
-  }
+    /**
+     * @return bool
+     */
+    public function hasAnySuccess()
+    {
+        return $this->hasAny(self::TYPE_SUCCESS);
+    }
 
-  /**
-   * @param $key
-   * @param $value
-   * @return $this
-   */
-  public function setInfo($key, $value)
-  {
-    return $this->set(self::TYPE_INFO, $key, $value);
-  }
+    /**
+     * @return array
+     */
+    public function getAllSuccess()
+    {
+        return $this->getAll(self::TYPE_SUCCESS);
+    }
 
-  /**
-   * @param $key
-   * @return bool
-   */
-  public function hasInfo($key)
-  {
-    return $this->has(self::TYPE_INFO, $key);
-  }
+    /**
+     * @param $key
+     * @return $this
+     */
+    public function removeSuccess($key)
+    {
+        return $this->remove(self::TYPE_SUCCESS, $key);
+    }
 
-  /**
-   * @return bool
-   */
-  public function hasAnyInfo()
-  {
-    return $this->hasAny(self::TYPE_INFO);
-  }
+    /**
+     * @return $this
+     */
+    public function removeAllSuccess()
+    {
+        return $this->removeAll(self::TYPE_SUCCESS);
+    }
 
-  /**
-   * @return array
-   */
-  public function getAllInfo()
-  {
-    return $this->getAll(self::TYPE_INFO);
-  }
+    /**
+     * @param $key
+     * @param $default
+     * @return mixed
+     */
+    public function getInfo($key, $default = null)
+    {
+        return $this->get(self::TYPE_INFO, $key, $default);
+    }
 
-  /**
-   * @param $key
-   * @return $this
-   */
-  public function removeInfo($key)
-  {
-    return $this->remove(self::TYPE_INFO, $key);
-  }
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setInfo($key, $value)
+    {
+        return $this->set(self::TYPE_INFO, $key, $value);
+    }
 
-  /**
-   * @return $this
-   */
-  public function removeAllInfo()
-  {
-    return $this->removeAll(self::TYPE_INFO);
-  }
-  
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function hasInfo($key)
+    {
+        return $this->has(self::TYPE_INFO, $key);
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasAnyInfo()
+    {
+        return $this->hasAny(self::TYPE_INFO);
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllInfo()
+    {
+        return $this->getAll(self::TYPE_INFO);
+    }
+
+    /**
+     * @param $key
+     * @return $this
+     */
+    public function removeInfo($key)
+    {
+        return $this->remove(self::TYPE_INFO, $key);
+    }
+
+    /**
+     * @return $this
+     */
+    public function removeAllInfo()
+    {
+        return $this->removeAll(self::TYPE_INFO);
+    }
+
 }
